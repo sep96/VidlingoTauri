85028cf1b7c992f8dbe6c8ea1c9097887f7e3522
James <sep7696@gmail.com>
2025-07-10 12:29:56 +0330

    feat : add ffmpeg libraray to main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use ffmpeg_next as ffmpeg;
 
 fn main() {
     vidlingo_lib::run()
+}
+
+#[tauri::command]
+fn get_ffmpeg_version() -> String {
+    ffmpeg::init().unwrap();
+    ffmpeg::version::string().to_string()
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_ffmpeg_version])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
